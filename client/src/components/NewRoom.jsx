f13c24fc717203b268443d89d3fcf790c51efc0f
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-11-08 10:06:03 -0500

    Added new room function

--- client/src/components/NewRoom.jsx
+++ client/src/components/NewRoom.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
 
 export default class NewRoom extends Component {
+    postNewRoom = (evt) => {
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'))
+        let newRoom = {
+            name: evt.target.name.value,
+            creatorId: currentUser._id
+        }
+        Axios.post('/rooms', newRoom)
+        .then((res) => {
+            res.redirect(`/rooms/${res._id}`)
+        })
+    }
     render() {
         return (
-            <form>
-                <input type='text' placeholder='Room Name'/>
+            <form onSubmit={this.postNewRoom}>
+                <input type='text' name='name' placeholder='Room Name'/>
                 <input type='submit' value='Create Room'/>
             </form>
         )
