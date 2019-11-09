77d9401b009aa987529f1acda8163a6b61719a54
Noah Rorabaugh <noahrorabaugh@Noahs-MBP.attlocal.net>
2019-11-09 17:35:37 -0500

    Handled redirect

--- client/src/components/Mudroom.jsx
+++ client/src/components/Mudroom.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default class Mudroom extends Component {
     componentDidMount() {
@@ -6,12 +7,7 @@ export default class Mudroom extends Component {
     }
     render() {
         return(
-            <div>
-                <p>Welcome to VentChat, where you can vent about anything you want!</p>
-                <a href='/rooms'>
-                    <button>Enter VentChat</button>
-                </a>
-            </div>
+            <Redirect to='/rooms'/>
         )
     }
 }
