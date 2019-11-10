eb89328a9891eae952f5effdff383e050c4d7188
Noah Rorabaugh <noahrorabaugh@Noahs-MBP.attlocal.net>
2019-11-10 11:01:14 -0500

    Fixed message count

--- client/src/components/RoomListItem.jsx
+++ client/src/components/RoomListItem.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class RoomListItem extends Component {
+    state = {
+        messages: [],
+    }
+    componentDidMount() {
+        axios.get(`/api/messages/room/${this.props.id}`)
+        .then((response) => {
+            this.setState({messages: response.data})
+        })
+    }
+
     render() {
         let link = `/rooms/${this.props.id}`
         return (
             <div className='listItem'>
                 <a href={link}>
                     <h2>{this.props.name}</h2>
-                    <p>{this.props.messageCount} messages</p>
+                    <p>{this.state.messages.length} {this.state.messages.length == 1? 'message' : 'messages'}</p>
                 </a>
             </div>
         )
