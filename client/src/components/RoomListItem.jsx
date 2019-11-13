a09ee9a5f550053d8f617fe410905ae78652e7b7
Noah Rorabaugh <noahrorabaugh@Noahs-MBP.attlocal.net>
2019-11-12 20:05:54 -0500

    Removed room delete button

--- client/src/components/RoomListItem.jsx
+++ client/src/components/RoomListItem.jsx
@@ -12,17 +12,21 @@ export default class RoomListItem extends Component {
         })
     }
 
+    deleteRoom() {
+        axios.delete(`/api/messages/room/${this.props.id}`)
+        .then(() => {
+            this.render()
+        })
+    }
+
     render() {
         let link = `/rooms/${this.props.id}`
         let account = JSON.parse(localStorage.getItem('currentUser')).userName === this.props.creatorName
         return (
             <a href={link}>
             <div className='listItem'>
-                    <h2>{this.props.name}</h2>
-                    <div className='host'>                   
-                        <p>Hosted by {account? 'you' : this.props.creatorName}</p>
-                        {account? <button className='delete'>Delete Room</button> : null}
-                    </div>
+                    <h2>{this.props.name}</h2>                   
+                    <p>Hosted by {account? 'you' : this.props.creatorName}</p>
                     <p>{this.state.messages.length} {this.state.messages.length === 1? 'message' : 'messages'}</p>                       
             </div>
             </a>
