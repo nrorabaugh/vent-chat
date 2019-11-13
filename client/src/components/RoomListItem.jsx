43a9b12b18755d7a88af4429a8818051104e0173
Noah Rorabaugh <noahrorabaugh@Noahs-MBP.attlocal.net>
2019-11-12 20:01:09 -0500

    Room delete  button

--- client/src/components/RoomListItem.jsx
+++ client/src/components/RoomListItem.jsx
@@ -18,8 +18,11 @@ export default class RoomListItem extends Component {
         return (
             <a href={link}>
             <div className='listItem'>
-                    <h2>{this.props.name}</h2>                   
-                    <p>Hosted by {account? 'you' : this.props.creatorName}</p>
+                    <h2>{this.props.name}</h2>
+                    <div className='host'>                   
+                        <p>Hosted by {account? 'you' : this.props.creatorName}</p>
+                        {account? <button className='delete'>Delete Room</button> : null}
+                    </div>
                     <p>{this.state.messages.length} {this.state.messages.length === 1? 'message' : 'messages'}</p>                       
             </div>
             </a>
